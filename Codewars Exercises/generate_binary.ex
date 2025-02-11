b479f1bdf78481a648c65fcc43c2081df51d2528
AlvisonHunterArnuero <alvison@gmail.com>
2025-02-11 01:07:10 -0600

    chore(exercises): added more exercises to this list

---
+++ Codewars Exercises/generate_binary.ex
@@ -0,0 +1,14 @@
+defmodule FakeBinary do
+  def generate(str) do
+  str
+  |> String.graphemes()
+      |> Enum.map(fn char ->
+      case Integer.parse(char) do
+        {number, _} when number > 4 -> "1"
+        {number, _} when number < 5 -> "0"
+        _ -> char
+      end
+    end)
+    |> Enum.join("")
+  end
+end
